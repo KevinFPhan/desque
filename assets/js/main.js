cdf74789e28669ea156b36edbac92ee5d91b0a2c
Kevin Phan <90020140+KevinFPhan@users.noreply.github.com>
2022-06-08 00:47:03 -0700

    asd

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,6 @@
 			imJs.unloadImage3();
 			imJs.darkLight();
 			imJs.vedioActivation();
-			imJs.tiltJS();
 		},
 
 		featherAtcivation: function () {
@@ -845,32 +844,13 @@
 					.remove();
 			}
 		},
-		tiltJS: function (e) {
-			const tilt = document.querySelectorAll('.tilt');
-
-			VanillaTilt.init(tilt, {
-				reverse: true,
-				max: 3,
-				speed: 400,
-				// glare: true,
-				reset: true,
-				perspective: 500,
-				transition: true,
-				'max-glare': 0.75,
-				'glare-prerender': false,
-				gyroscope: true,
-				gyroscopeMinAngleX: -45,
-				gyroscopeMaxAngleX: 45,
-				gyroscopeMinAngleY: -45,
-				gyroscopeMaxAngleY: 45,
-			});
-		},
 	};
 
 	imJs.m();
 })(jQuery, window);
 
 const activePage = window.location.pathname;
+console.log(activePage);
 const navLinks = document.querySelectorAll('nav a').forEach((link) => {
 	if (link.href.includes(`${activePage}`)) {
 		link.classList.add('active-nav');
